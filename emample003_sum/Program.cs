0d22052c951d1ffb949f03c9f556fcdcf9cd78a6
katep <katepsupport@mail.com>
2023-03-22 23:28:31 +0400

    Random3

---
+++ emample003_sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,10); //1 2 3 4 5 ...9
+Console.Write(numberA);
+int numberB = new Random().Next(1,10);
+Console.Write(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
